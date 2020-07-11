4ffc8d8835f42970e4b3fd93bf1ead584224927a
Jean Paul Barrit <jpbr25@yahoo.com>
2020-07-11 01:35:42 -0600

    Actualización a la página de perfil

--- src/app/perfil/perfil.component.ts
+++ src/app/perfil/perfil.component.ts
@@ -7,6 +7,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PerfilComponent implements OnInit {
 
+  fills=[{value:"Jean Paul",label:"Nombre"},
+        {value:"Barrit",label:"Apellido"},
+        {value:"jpaulbr97@gmail.com",label:"Correo"},
+        {value:"*********",label:"Contraseña"}
+  ];
+  materials=[
+    {img:"assets/imagenes/botella3.png",label:"Plástico",value:"0 kg"},
+    {img:"assets/imagenes/papel3.png",label:"Papel",value:"0 kg"},
+    {img:"assets/imagenes/lata3.png",label:"Aluminio",value:"0 kg"},
+    {img:"assets/imagenes/vidrio3.png",label:"Vidrio",value:"0 kg"},
+    {img:"assets/imagenes/tetra3.png",label:"Tetre pack",value:"0 kg"},
+    {img:"assets/imagenes/bateria3.png",label:"Batería",value:"0 kg"}
+  ]
+
   constructor() { }
 
   ngOnInit(): void {
